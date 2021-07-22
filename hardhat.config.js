09a7d2c58daca7b0b44e990734bd27fd995212c4
Tony <tony@maapps.dev>
2021-07-22 17:09:48 -0400

    Disable network configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -14,20 +14,20 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-const INFURA_PROJECT_ID = "e83a710ee610460882fc25ae775ecbee";
-const fs = require("fs");
-const PRIVATE_KEY = fs.readFileSync(".secret").toString().trim();
+// const INFURA_PROJECT_ID = "e83a710ee610460882fc25ae775ecbee";
+// const fs = require("fs");
+// const PRIVATE_KEY = fs.readFileSync(".secret").toString().trim();
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.4",
-  networks: {
-    hardhat: {},
-    kovan: {
-      url: `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [PRIVATE_KEY],
-    },
-  },
+  // networks: {
+  //   hardhat: {},
+  //   kovan: {
+  //     url: `https://kovan.infura.io/v3/${INFURA_PROJECT_ID}`,
+  //     accounts: [PRIVATE_KEY],
+  //   },
+  // },
 };
